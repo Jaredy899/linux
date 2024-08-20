ef58ac8da947e80661fcb7eef0fd27fcd4c127a9
Jaredy899 <76603258+Jaredy899@users.noreply.github.com>
2024-08-20 10:15:14 -0400

    Update fix_bashrc.sh

--- fix_bashrc.sh
+++ fix_bashrc.sh
@@ -3,7 +3,7 @@
 # Define the lines to be added
 line4="# Determine if the shell is interactive"
 line5="if [[ \$iatest -gt 0 ]]; then"
-line639="fi"
+line_end="fi"
 
 # File path
 file="$HOME/linuxtoolbox/mybash/.bashrc"
@@ -17,9 +17,14 @@ insert_line_at_position() {
     sed -i "${position}i $line" "$file"
 }
 
-# Add the lines
+# Add the lines at specific positions
 insert_line_at_position "$line4" 4 "$file"
 insert_line_at_position "$line5" 5 "$file"
-insert_line_at_position "$line639" 639 "$file"
+
+# Check if the file already ends with 'fi' and remove it if present to avoid duplicates
+sed -i '$s/^\s*fi\s*$//' "$file"
+
+# Append "fi" at the end of the file
+echo "$line_end" >> "$file"
 
 echo "Lines added successfully to $file."
