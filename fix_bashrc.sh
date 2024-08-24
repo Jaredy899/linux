150ad5df18f868abd7bd368493b83a43f7c01a33
Jaredy899 <76603258+Jaredy899@users.noreply.github.com>
2024-08-23 23:16:34 -0400

    Update fix_bashrc.sh

--- fix_bashrc.sh
+++ fix_bashrc.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-# Define the lines to be added
+# Define the lines to be added for interactive shell check
 line4="# Determine if the shell is interactive"
 line5="if [[ \$iatest -gt 0 ]]; then"
 line_end="fi"
 
 # File path
 file="$HOME/linuxtoolbox/mybash/.bashrc"
+
+# Define the alias line to be added
+alias_line="alias ff='fastfetch -c all'"
 
 # Function to insert lines at a specific position
 insert_line_at_position() {
@@ -16,6 +19,14 @@ insert_line_at_position() {
 
     sed -i "${position}i $line" "$file"
 }
+
+# Check if the alias already exists in the .bashrc file and add it if not
+if ! grep -q "$alias_line" "$file"; then
+    echo "$alias_line" >> "$file"
+    echo "Alias added successfully to $file."
+else
+    echo "Alias already exists in $file."
+fi
 
 # Add the lines at specific positions
 insert_line_at_position "$line4" 4 "$file"
