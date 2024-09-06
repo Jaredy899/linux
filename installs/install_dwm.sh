3f6d89459ff2fa1cbfb33660702be48595a06b18
Jaredy899 <76603258+Jaredy899@users.noreply.github.com>
2024-09-06 17:19:32 -0400

    fix?

--- installs/install_dwm.sh
+++ installs/install_dwm.sh
@@ -44,16 +44,17 @@ install_packages() {
     case "$distro" in
         ubuntu|debian)
             packages="nano thunar vlc pulseaudio alsa-utils pavucontrol fonts-firacode network-manager-gnome feh"
+            
             if ! command -v wget >/dev/null 2>&1; then
                 echo "Installing wget..."
                 sudo apt-get install -y wget
             fi
 
             echo "Adding Mozilla APT repository and installing Firefox..."
-            sudo install -d -m 0755 /etc/apt/keyrings
-            wget -q https://packages.mozilla.org/apt/repo-signing-key.gpg -O- | sudo tee /etc/apt/keyrings/packages.mozilla.org.asc > /dev/null
-            gpg -n -q --import --import-options import-show /etc/apt/keyrings/packages.mozilla.org.asc | awk '/pub/{getline; gsub(/^ +| +$/,""); if($0 == "35BAA0B33E9EB396F59CA838C0BA5CE6DC6315A3") print "\nThe key fingerprint matches ("$0").\n"; else print "\nVerification failed: the fingerprint ("$0") does not match the expected one.\n"}'
-            echo "deb [signed-by=/etc/apt/keyrings/packages.mozilla.org.asc] https://packages.mozilla.org/apt mozilla main" | sudo tee -a /etc/apt/sources.list.d/mozilla.list > /dev/null
+            sudo mkdir -p /etc/apt/keyrings
+            wget -q https://packages.mozilla.org/apt/repo-signing-key.gpg -O /etc/apt/keyrings/mozilla-archive-keyring.gpg
+
+            echo "deb [signed-by=/etc/apt/keyrings/mozilla-archive-keyring.gpg] https://packages.mozilla.org/apt mozilla main" | sudo tee /etc/apt/sources.list.d/mozilla.list > /dev/null
             echo '
             Package: *
             Pin: origin packages.mozilla.org
@@ -62,6 +63,10 @@ install_packages() {
 
             sudo apt-get update -y
             sudo apt-get install -y $packages firefox
+            ;;
+        *)
+            echo "Unsupported distribution: $distro"
+            exit 1
             ;;
         fedora|centos|rhel)
             packages="nano thunar vlc pipewire pipewire-alsa pipewire-pulseaudio alsa-utils pavucontrol fira-code-fonts NetworkManager-tui firefox feh"
