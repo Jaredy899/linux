ab4df501d4e6d7617374cc36d64c9583c31579e2
Jaredy899 <76603258+Jaredy899@users.noreply.github.com>
2024-09-25 18:54:08 -0400

    meh

--- tests/btrfs.sh
+++ tests/btrfs.sh
@@ -18,3 +18,15 @@ btrfs subvolume create "$MOUNT_POINT/@home"
 echo "Created home subvolume: @home"
 
 echo "BTRFS subvolumes created successfully."
+
+# Add entries to /etc/fstab
+echo "Adding subvolume entries to /etc/fstab..."
+
+# Get the UUID of the BTRFS filesystem
+UUID=$(blkid -s UUID -o value $(findmnt -no SOURCE "$MOUNT_POINT"))
+
+# Append entries to /etc/fstab
+echo "UUID=$UUID  /          btrfs   subvol=@,defaults,noatime  0  1" >> /etc/fstab
+echo "UUID=$UUID  /home      btrfs   subvol=@home,defaults,noatime  0  2" >> /etc/fstab
+
+echo "Entries added to /etc/fstab successfully."
