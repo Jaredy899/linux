79422cf26d02399339de1da2a92c02328150a6cf
Jaredy899 <76603258+Jaredy899@users.noreply.github.com>
2024-10-09 19:14:59 -0400

    dev

--- installs/cockpit.sh
+++ installs/cockpit.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -e
 
 # Source the common script directly from GitHub
-. <(curl -s https://raw.githubusercontent.com/Jaredy899/linux/refs/heads/main/common_script.sh)
+. <(curl -s https://raw.githubusercontent.com/Jaredy899/linux/refs/heads/dev/common_script.sh)
 
 # Run the environment check
 checkEnv || exit 1
