ecfe636bbbe01eb4bfe62f2a9ef193d6d89a9d41
Jaredy899 <76603258+Jaredy899@users.noreply.github.com>
2024-11-02 18:22:57 -0400

    xorg

--- installs/de-installer.sh
+++ installs/de-installer.sh
@@ -6,10 +6,10 @@
 # Function to install desktop environment on Arch
 install_arch_de() {
     case $1 in
-        1) noninteractive cinnamon lightdm lightdm-gtk-greeter ;;
-        2) noninteractive plasma plasma-wayland-protocols plasma-desktop sddm plasma-pa plasma-nm konsole dolphin ;;
-        3) noninteractive gnome gnome-extra gdm ;;
-        4) noninteractive i3-gaps i3status i3blocks dmenu lightdm lightdm-gtk-greeter ;;
+        1) noninteractive cinnamon lightdm lightdm-gtk-greeter xorg-server ;;
+        2) noninteractive plasma plasma-wayland-protocols plasma-desktop sddm plasma-pa plasma-nm konsole dolphin xorg-server ;;
+        3) noninteractive gnome gnome-extra gdm xorg-server ;;
+        4) noninteractive i3-gaps i3status i3blocks dmenu lightdm lightdm-gtk-greeter xorg-server ;;
     esac
 }
 
