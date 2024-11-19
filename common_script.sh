5dce222a01968d5b71f8b26313b34e77ec3cd88f
Jaredy899 <76603258+Jaredy899@users.noreply.github.com>
2024-11-18 22:04:44 -0500

    checkarch

--- common_script.sh
+++ common_script.sh
@@ -12,6 +12,16 @@ command_exists() {
     command -v "$1" >/dev/null 2>&1
 }
 
+checkArch() {
+    case "$(uname -m)" in
+        x86_64 | amd64) ARCH="x86_64" ;;
+        aarch64 | arm64) ARCH="aarch64" ;;
+        *) printf "%b\n" "${RED}Unsupported architecture: $(uname -m)${RC}" && exit 1 ;;
+    esac
+
+    printf "%b\n" "${CYAN}System architecture: ${ARCH}${RC}"
+}
+
 checkAURHelper() {
     ## Check & Install AUR helper
     if [ "$PACKAGER" = "pacman" ] && [ -z "$SKIP_AUR_CHECK" ]; then
