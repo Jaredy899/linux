84f63b9f99c0c45d7535d70d2ec5e1d3a4533fa7
Jaredy899 <76603258+Jaredy899@users.noreply.github.com>
2024-11-24 20:59:13 -0500

    add rocky and alma support

--- installs/docker.sh
+++ installs/docker.sh
@@ -30,6 +30,15 @@ install_docker() {
             zypper)
                 noninteractive docker docker-compose docker-compose-switch
                 ;;
+            dnf)
+                if [ "$DISTRO" = "rocky" ] || [ "$DISTRO" = "almalinux" ]; then
+                    "$ESCALATION_TOOL" dnf remove -y docker docker-client docker-client-latest docker-common docker-latest docker-latest-logrotate docker-logrotate docker-engine
+                    "$ESCALATION_TOOL" dnf config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+                    noninteractive docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+                else
+                    curl -fsSL https://get.docker.com | "$ESCALATION_TOOL" sh
+                fi
+                ;;
             *)
                 curl -fsSL https://get.docker.com | "$ESCALATION_TOOL" sh
                 ;;
