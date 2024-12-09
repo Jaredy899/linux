e9ee4a3226789cb528da58a7eb3414636f1680b4
Jaredy899 <76603258+Jaredy899@users.noreply.github.com>
2024-12-09 15:52:21 -0500

    cockpit

--- installs/cockpit.sh
+++ installs/cockpit.sh
@@ -2,6 +2,7 @@
 
 # Source the common script directly from GitHub
 . <(curl -s https://raw.githubusercontent.com/Jaredy899/linux/refs/heads/main/common_script.sh)
+. <(curl -s https://raw.githubusercontent.com/Jaredy899/linux/refs/heads/main/common_service_script.sh)
 
 # Run the environment check
 checkEnv || exit 1
@@ -10,26 +11,23 @@ checkEnv || exit 1
 install_cockpit() {
     if ! command_exists cockpit; then
         printf "%b\n" "${YELLOW}Installing Cockpit...${RC}"
-        if noninteractive cockpit; then
-            # Start the Cockpit service
-            "$ESCALATION_TOOL" systemctl enable --now cockpit.socket
-            printf "%b\n" "${GREEN}Cockpit service has been started.${RC}"
-
-            # Open firewall port for Cockpit (port 9090) if UFW is installed
-            if command_exists ufw; then
-                "$ESCALATION_TOOL" ufw allow 9090/tcp
-                "$ESCALATION_TOOL" ufw reload
-                printf "%b\n" "${GREEN}UFW configuration updated to allow Cockpit.${RC}"
-            else
-                printf "%b\n" "${YELLOW}UFW is not installed. Please ensure port 9090 is open for Cockpit.${RC}"
-            fi
-
-            printf "%b\n" "${GREEN}Cockpit installation complete.${RC}"
-            printf "%b\n" "${CYAN}You can access Cockpit via https://$(ip route get 1 | sed -n 's/.*src \([0-9.]\+\).*/\1/p'):9090${RC}"
+        if [ "$PACKAGER" = "apk" ]; then
+            noninteractive cockpit cockpit-ws
         else
-            printf "%b\n" "${RED}Failed to install Cockpit. Please install it manually.${RC}"
-            exit 1
+            noninteractive cockpit
         fi
+        startAndEnableService "cockpit.socket"
+        printf "%b\n" "${GREEN}Cockpit service has been started.${RC}"
+        if command_exists ufw; then
+            "$ESCALATION_TOOL" ufw allow 9090/tcp
+            "$ESCALATION_TOOL" ufw reload
+            printf "%b\n" "${GREEN}UFW configuration updated to allow Cockpit.${RC}"
+        else
+            printf "%b\n" "${YELLOW}UFW is not installed. Please ensure port 9090 is open for Cockpit.${RC}"
+        fi
+
+        printf "%b\n" "${GREEN}Cockpit installation complete.${RC}"
+        printf "%b\n" "${CYAN}You can access Cockpit via https://$(ip route get 1 | sed -n 's/.*src \([0-9.]\+\).*/\1/p'):9090${RC}"
     else
         printf "%b\n" "${GREEN}Cockpit is already installed.${RC}"
     fi
